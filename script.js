2dca89ec32237dfdaf31441d1c76f0f2e5e324e6
Alex-Kinzoky <90065458+Alex-Kinzoky@users.noreply.github.com>
2023-03-19 22:17:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    //место для кода
+    anime({
+        targets: '.screw',
+        rotate: 360,
+        duration: 2000,
+        easing: 'linear',
+        loop:true
+    })
+})
